2249322c549b8d6a8c19b5cbd9d49fe8c0da5cb4
Aleksandra Kalimancevic <ak20180179@student.fon.bg.ac.rs>
2021-12-27 14:59:10 +0100

    Izmena fajla knjiga.php, dodate nove funkcije

--- model/knjiga.php
+++ model/knjiga.php
@@ -1,4 +1,5 @@
 <?php
+require "../baza.php";
 class Knjiga{
     public $id;   
     public $naziv;   
@@ -13,6 +14,29 @@ class Knjiga{
         $this->brstrana = $brstrana;
         $this->autor = $autor;
         $this->oblast = $oblast;
+    }
+
+
+    public static function deleteById($knjiga)
+    {
+        $dodaj=new Database("biblioteka");
+        $podaci=array($knjiga->id);
+        return $dodaj->delete("knjiga", $podaci);
+    }
+
+    public static function update($knjiga)
+    {
+        $dodaj=new Database("biblioteka");
+        $podaci=array($knjiga->id, $knjiga->brstrana);
+        return $dodaj->update("knjiga", $podaci);
+    }
+
+    
+    public static function add($knjiga)
+    {
+        $dodaj=new Database("biblioteka");
+        $podaci=array($knjiga->naziv, $knjiga->brstrana, $knjiga->autor, $knjiga->oblast);
+        return $dodaj->insert("knjiga", "knjiganaziv, knjigabrstrana, knjigaautorid, knjigaoblastid", $podaci);
     }
 }
 
