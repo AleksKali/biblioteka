884fa4c7c75f6509f0dea63c2f1102951e0ca2c4
Aleksandra Kalimancevic <ak20180179@student.fon.bg.ac.rs>
2021-12-27 15:32:52 +0100

    Omoguceno je sortiranje tabele

--- home.php
+++ home.php
@@ -88,7 +88,12 @@ $izbrisiknjigu=new Database("biblioteka");
                         <?php  } ?>
                 </tbody>
             </table>
-           
+            <div class="row">
+                    <div class="col-md-2" style="text-align: right;">
+                    <button id="btn-sortiraj" class="btn btn-normal" onclick="sortTable()">Sortiraj</button>
+                </div>
+
+            </div>
     
             </div>
     <?php
@@ -96,7 +101,33 @@ $izbrisiknjigu=new Database("biblioteka");
 $prikaziautore->select_autore();
 $prikazioblast->select_oblast();
 ?>
-   
+   <script>
+        function sortTable() {
+            var table, rows, switching, i, x, y, shouldSwitch;
+            table = document.getElementById("myTable");
+            switching = true;
+
+            while (switching) {
+                switching = false;
+                rows = table.rows;
+                for (i = 1; i < (rows.length - 1); i++) {
+                    shouldSwitch = false;
+                    x = rows[i].getElementsByTagName("TD")[3];
+                    y = rows[i + 1].getElementsByTagName("TD")[3];
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+                if (shouldSwitch) {
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                }
+            }
+        }
+
+    </script>
+
 
 </body>
 
