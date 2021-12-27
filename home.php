d56f8ef3f8cf2b9ea2f2b3d553f02be84014e70a
Aleksandra Kalimancevic <ak20180179@student.fon.bg.ac.rs>
2021-12-27 15:35:56 +0100

    Dodate forme na home.php

--- home.php
+++ home.php
@@ -101,6 +101,171 @@ $izbrisiknjigu=new Database("biblioteka");
 $prikaziautore->select_autore();
 $prikazioblast->select_oblast();
 ?>
+
+<div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-forma-form">
+                        <form action="#" method="post" id="dodajForm"> 
+                            <h3 style="color: black; text-align: center">Dodaj knjigu</h3>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <h3>Naziv:</h3>
+                                        <input id="naziv" type="text" style="border: 1px solid black" name="naziv" class="form-txt" />
+                                    </div>
+                                    <div class="form-group">
+                                        <h3>Broj strana:</h3>
+                                        <input type="text" style="border: 1px solid black" name="brstrana" class="form-txt" />
+                                    </div>
+                                    <div class="form-group">
+                                    <h3>Autor:</h3>
+                                                <select class="search-filed" name="autor">
+                                                <option value="">Izaberite autora...</option>
+                                                <?php while ($red=$prikaziautore->getResult()->fetch_object()) {  ?>
+                                                            <option value="<?php echo $red->autorid; ?>"><?php echo $red->autorime .' '. $red->autorprezime; ?></option>
+                                                            <?php  } ?>
+                                                </select><br>
+                                    </div>
+                                    <div><br>
+                                        <div class="form-group">
+                                        <h3>Oblast:</h3>
+                                                <select class="search-filed" name="oblast">
+                                                <option value="">Izaberite oblast...</option>
+                                                <?php while ($red2=$prikazioblast->getResult()->fetch_object()) {  ?>
+                                                            <option value="<?php echo $red2->oblastid; ?>"><?php echo $red2->oblastnaziv;?></option>
+                                                            <?php  } ?>
+                                                </select><br>
+                                        </div><br>
+                                    </div><br>
+                                    <div class="form-group">
+                                        <button id="btnDodaj" type="submit" class="btn btn-success btn-block" tyle="background-color: orange; border: 1px solid black;">Dodaj</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+<?php
+$izmeniknjigu->select();
+?>
+    </div>
+    <div class="modal fade" id="izmeniModal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-forma-form">
+                        <form action="#" method="post" id="izmeniForm">
+                            <h1 style="color: black">Izmeni knjigu</h1>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                    <h3>Knjiga:</h3><br>
+					   <select class="search-filed" name="id">
+					   <option value="">Izaberite knjigu...</option>
+					  
+					   <?php while ($red=$izmeniknjigu->getResult()->fetch_object()) {  ?>
+					   			<option value="<?php echo $red->knjigaid; ?>"><?php echo $red->knjiganaziv; ?></option>
+					   			<?php  } ?>
+					   </select><br>
+                                    </div>
+                                    <div class="form-group"><br>
+                                    <h3>Broj strana:</h3><br>
+					                    <input name="brstrana" type="text" class="search-filed" placeholder="Unesite novi broj strana..."><br>
+                                    </div><br>
+                                     <div class="form-group"><br>
+                                        <button id="btnIzmeni" type="submit" class="btn btn-success btn-block" style="color: white; background-color: orange; border: 1px solid white"> Izmeni
+                                        </button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="modal-footer" id="ftr">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Zatvori</button>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+
+    <?php
+$izbrisiknjigu->select();
+?>
+    </div>
+    <div class="modal fade" id="izbrisiModal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-forma-form">
+                        <form action="#" method="post" id="izbrisiForm">
+                            <h1 style="color: black">Iznajmi knjigu</h1>
+                            <h3 style="color: black">Knjiga:</h3>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                    <select class="search-filed" name="id">
+                                    <option value="">Izaberite knjigu...</option>
+                                    
+                                    <?php while ($red=$izbrisiknjigu->getResult()->fetch_object()) {  ?>
+                                                <option value="<?php echo $red->knjigaid; ?>"><?php echo $red->knjiganaziv; ?></option>
+                                                <?php  } ?>
+                                    </select><br>
+                                    </div><br><br>
+                                <div class="form-group"><br>
+                                        <button id="btnIzmeni" type="submit" class="btn btn-success btn-block" style="color: white; background-color: orange; border: 1px solid white"> Iznajmi
+                                        </button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div><br><br>
+                <div class="modal-footer" id="delftr">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Zatvori</button>
+                </div>
+            </div>
+ </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <script src="js/main.js"></script>
+
    <script>
         function sortTable() {
             var table, rows, switching, i, x, y, shouldSwitch;
